b3bbfd9855e16b1ba2841488b7f31410dd4eb09e
Aaron Rodriguez <aaroner@yahoo.com>
2019-11-12 17:20:31 -0600

    prog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `An Epic Gatsby Site`,
+    description: `First dive into working with Gatsby`,
+    author: `@AHeroOfTime`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,5 +30,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'posts',
+      },
+    },
   ],
-}
+};
---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Yes! In your face, Gandhi! Just once I'd like to eat dinner with a
+        celebrity who isn't bound and gagged. Tell her she looks thin. I am the
+        man with no name, Zapp Brannigan! As an interesting side note, as a head
+        without a body, I envy the dead. I just told you! You've killed me! Say
+        what? Or a guy who burns down a bar for the insurance money! Of all the
+        friends I've had… you're the first. I'm sure those windmills will keep
+        them cool. Daddy Bender, we're hungry. Say what? OK, this has gotta
+        stop. I'm going to remind Fry of his humanity the way only a woman can.
+        All I want is to be a monkey of moderate intelligence who wears a suit…
+        that's why I'm transferring to business school!
+      </p>
+    </div>
+  </Layout>
+);
+
+export default About;
